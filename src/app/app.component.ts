44e1ce0dd9e6cc5620b2e33cb1af197c293e9c04
Ricardo Pedrosa <rickpedrosa94@gmail.com>
2020-04-28 21:04:57 +0200

    Flexbox inside betslip achieved?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,19 @@ export class AppComponent {
   title = 'sass-demo';
   clubs = [];
   betsDemoArray = [
-    {
-      index: 0,
-      header : 'Suggestion Box',
-      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer eget ornare sapien. Nunc feugiat accumsan ligula, ac venenatis enim. Ut gravida sapien lacinia nisl porta pulvinar. Nunc condimentum hendrerit tortor interdum ultricies. Nam ac molestie leo, luctus lobortis lacus. Sed eu ex vel sem dictum commodo. Praesent tincidunt pellentesque ante eget suscipit. Mauris quis lectus nunc. Aliquam congue in mi ut mattis. Proin tristique accumsan libero, ut dictum nunc auctor quis. Morbi ac tempor purus. Quisque id turpis nec nulla ornare fringilla. Maecenas ultricies facilisis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer eget ornare sapien. Nunc feugiat accumsan ligula, ac venenatis enim. Ut gravida sapien lacinia nisl porta pulvinar. Nunc condimentum hendrerit tortor interdum ultricies. Nam ac molestie leo, luctus lobortis lacus. Sed eu ex vel sem dictum commodo. Praesent tincidunt pellentesque ante eget suscipit. Mauris quis lectus nunc. Aliquam congue in mi ut mattis. Proin tristique accumsan libero, ut dictum nunc auctor quis. Morbi ac tempor purus. Quisque id turpis nec nulla ornare fringilla. Maecenas ultricies facilisis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer eget ornare sapien. Nunc feugiat accumsan ligula, ac venenatis enim. Ut gravida sapien lacinia nisl porta pulvinar. Nunc condimentum hendrerit tortor interdum ultricies. Nam ac molestie leo, luctus lobortis lacus. Sed eu ex vel sem dictum commodo. Praesent tincidunt pellentesque ante eget suscipit. Mauris quis lectus nunc. Aliquam congue in mi ut mattis. Proin tristique accumsan libero, ut dictum nunc auctor quis. Morbi ac tempor purus. Quisque id turpis nec nulla ornare fringilla. Maecenas ultricies facilisis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer eget ornare sapien. Nunc feugiat accumsan ligula, ac venenatis enim. Ut gravida sapien lacinia nisl porta pulvinar. Nunc condimentum hendrerit tortor interdum ultricies. Nam ac molestie leo, luctus lobortis lacus. Sed eu ex vel sem dictum commodo. Praesent tincidunt pellentesque ante eget suscipit. Mauris quis lectus nunc. Aliquam congue in mi ut mattis. Proin tristique accumsan libero, ut dictum nunc auctor quis. Morbi ac tempor purus. Quisque id turpis nec nulla ornare fringilla. Maecenas ultricies facilisis ornare.',
-      expanded: true,
-      class: 'container-item collapsedLol'
-    }, {
-      index: 1,
-      header : 'Betslip Box',
-      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer eget ornare sapien. Nunc feugiat accumsan ligula, ac venenatis enim. Ut gravida sapien lacinia nisl porta pulvinar. Nunc condimentum hendrerit tortor interdum ultricies. Nam ac molestie leo, luctus lobortis lacus. Sed eu ex vel sem dictum commodo. Praesent tincidunt pellentesque ante eget suscipit. Mauris quis lectus nunc. Aliquam congue in mi ut mattis. Proin tristique accumsan libero, ut dictum nunc auctor quis. Morbi ac tempor purus. Quisque id turpis nec nulla ornare fringilla. Maecenas ultricies facilisis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer eget ornare sapien. Nunc feugiat accumsan ligula, ac venenatis enim. Ut gravida sapien lacinia nisl porta pulvinar. Nunc condimentum hendrerit tortor interdum ultricies. Nam ac molestie leo, luctus lobortis lacus. Sed eu ex vel sem dictum commodo. Praesent tincidunt pellentesque ante eget suscipit. Mauris quis lectus nunc. Aliquam congue in mi ut mattis. Proin tristique accumsan libero, ut dictum nunc auctor quis. Morbi ac tempor purus. Quisque id turpis nec nulla ornare fringilla. Maecenas ultricies facilisis ornare.',
-      expanded: false,
-      class: 'container-item collapsedLol'
-    }, {
-      index: 2,
-      header : 'Load bet Box',
-      name : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. IntegerLorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in. Pellentesque elementum turpis tempus pulvinar vulputate. Integer',
-      expanded: false,
-      class: 'container-item collapsedLol'
-    },
+    // {
+    //   index: 0,
+    //   header: 'Suggestion Box',
+    //   name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
+    //   expanded: false,
+    //   class: 'container-item collapsedLol'
+    // }, {
+    //   index: 1,
+    //   header: 'Betslip Box',
+    //   name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
+    //   expanded: false,
+    //   class: 'container-item collapsedLol'
+    // },
   ];
 
   constructor(private http: HttpClient) {
