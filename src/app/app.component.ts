454ad4c721b58b12b388a0fe63d737cbc9f3a855
Ricardo Pedrosa <rickpedrosa94@gmail.com>
2020-05-02 02:17:15 +0200

    Flexbox learned. Starting grid journey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 // @ts-ignore
 import {TeamByCategoryResponse} from './interfaces/teamByCategoryResponse';
+import {lorem} from './content/lorem';
 
 @Component({
   selector: 'app-root',
@@ -10,35 +11,44 @@ import {TeamByCategoryResponse} from './interfaces/teamByCategoryResponse';
 })
 export class AppComponent {
   title = 'sass-demo';
-  clubs = [];
   betsDemoArray = [
-    // {
-    //   index: 0,
-    //   header: 'Suggestion Box',
-    //   name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
-    //   expanded: false,
-    //   class: 'container-item collapsedLol'
-    // }, {
-    //   index: 1,
-    //   header: 'Betslip Box',
-    //   name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
-    //   expanded: false,
-    //   class: 'container-item collapsedLol'
-    // },
+    {
+      index: 0,
+      header: 'Suggestion Box',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
+      expanded: false,
+      class: 'container-item collapsedLol'
+    }, {
+      index: 1,
+      header: 'LoadBet Box',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
+      expanded: false,
+      class: 'container-item collapsedLol'
+    },
   ];
 
+  betSpecial = {
+    header: 'Betslip',
+    name: 'CONTENT\n' +
+      '        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.\n' +
+      '        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.\n' +
+      '        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit ante ex, quis molestie mi viverra in.',
+    expanded: true
+  };
+
+
   constructor(private http: HttpClient) {
-    this.http.get('http://localhost:8080/teams/golden', {
-      params: new HttpParams().append('page', '0')
-    }).subscribe(
-      (response: TeamByCategoryResponse) => {
-        this.clubs = response.content;
-        console.log(response.content);
-      },
-      error => {
-        console.error(error);
-      },
-      () => {
-      });
+    // this.http.get('http://localhost:8080/teams/golden', {
+    //   params: new HttpParams().append('page', '0')
+    // }).subscribe(
+    //   (response: TeamByCategoryResponse) => {
+    //     this.clubs = response.content;
+    //     console.log(response.content);
+    //   },
+    //   error => {
+    //     console.error(error);
+    //   },
+    //   () => {
+    //   });
   }
 }
